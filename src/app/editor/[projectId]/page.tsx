957440c3a7d34eb861dc34029c44bc5b60717617
Zeyu Chen <iszeyu.chen@gmail.com>
2024-07-06 00:27:54 +1000

    feat: editor layout

--- src/app/editor/[projectId]/page.tsx
+++ src/app/editor/[projectId]/page.tsx
@@ -1,4 +1,4 @@
-import { Editor } from '@/features/editor/components/editor';
+import { Editor } from '@/features/editor/components/Editor';
 
 const EditorProjectIdPage = () => {
   return (
---
+++ src/features/editor/components/Editor.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
+import { fabric } from 'fabric';
+
+import { Navbar } from '@/features/editor/components/Navbar';
+import { Sidebar } from '@/features/editor/components/Sidebar';
+import { Toolbar } from '@/features/editor/components/Toolbar';
+import { Footer } from '@/features/editor/components/Footer';
+import { useEditor } from '@/features/editor/hooks/use-editor';
+
+export const Editor = () => {
+  const {init} = useEditor();
+
+  const canvasRef = useRef(null);
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = new fabric.Canvas(
+      canvasRef.current,
+      {
+        controlsAboveOverlay: true,
+        preserveObjectStacking: true
+      }
+    );
+
+    init({
+      initialCanvas: canvas,
+      initialContainer: containerRef.current!
+    });
+
+    return () => {
+      canvas.dispose();
+    };
+  }, [init]);
+
+  return (
+    <div className="h-full flex flex-col">
+      <Navbar/>
+      <div className="absolute h-[calc(100%-68px)] w-full top-[68px] flex">
+        <Sidebar/>
+        <main className="bg-muted flex-1 overflow-auto relative flex flex-col">
+          <Toolbar/>
+          <div className="flex-1 h-[calc(100%-124px)] bg-muted" ref={containerRef}>
+            <canvas ref={canvasRef}/>
+          </div>
+          <Footer/>
+        </main>
+      </div>
+    </div>
+  );
+};
